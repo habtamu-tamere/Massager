32ba089b551505a8bf09fb7c291e0f40b85e1998
hax <56984134+habtamu-tamere@users.noreply.github.com>
2025-09-14 22:01:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -73,32 +73,4 @@ process.on('unhandledRejection', (err, promise) => {
   });
 });
 
-module.exports = app;    message: `Route ${req.originalUrl} not found`
-  });
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: 'Something went wrong!'
-  });
-});
-
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  // Close server & exit process
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
 module.exports = app;
